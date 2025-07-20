1154d287190342faa4ef24251a4b73a10ea904e7
Aakash <aakashsanjaysingh@gmail.com>
2025-07-21 01:20:29 +0530

    Refactored bubbleSort : used function and added complexity.

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,8 +2,12 @@ def bubbleSort(arr):
     for i in range(len(arr)):
         for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+                arr[j],arr[j+1] = arr[j+1],arr[j]
     return arr
+#complexity is 0(n^2) as there are 2 loops, one outer loop and one inner loop.
+
+# Example
+print(bubbleSort([64, 34, 25, 12, 22, 11, 90]))
 
 arr = list(map(int,input().split()))
 print(bubbleSort(arr))
