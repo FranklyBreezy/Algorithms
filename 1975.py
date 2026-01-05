b107cc2d2e6505e3f5012d9c8a03a76853a30089
Aakash <aakashsanjaysingh@gmail.com>
2026-01-05 17:57:12 +0530

    :3

---
+++ 1975.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxMatrixSum(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        total = 0
+        neg_count = 0
+        min_abs = float('inf')
+
+        for row in matrix:
+            for x in row:
+                total += abs(x)
+                if x < 0:
+                    neg_count += 1
+                min_abs = min(min_abs, abs(x))
+
+        if neg_count % 2 == 0:
+            return total
+        else:
+            return total - 2 * min_abs
