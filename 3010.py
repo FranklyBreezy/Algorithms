b5b8bfbed91aaf34e9c52af00acf98b8cd385178
Aakash <aakashsanjaysingh@gmail.com>
2026-02-01 18:19:01 +0530

    3010 soln

---
+++ 3010.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minimumCost(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        first = nums[0]
+        
+        smallest = float('inf')
+        second_smallest = float('inf')
+        
+        for x in nums[1:]:
+            if x < smallest:
+                second_smallest = smallest
+                smallest = x
+            elif x < second_smallest:
+                second_smallest = x
+        
+        return first + smallest + second_smallest
